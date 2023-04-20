24411a374bae06a4467acb301d95ad3d52a5754c
Nguyễn Tiến Mai <maint6@rikkeisoft.com>
2023-04-20 17:33:25 +0700

    no message

--- mai/MessageRow.swift
+++ mai/MessageRow.swift
@@ -10,14 +10,41 @@ import SwiftUI
 struct MessageRow: View {
     
     var message: Message
+    @State var isOnline: Bool = false
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack() {
+
+            ZStack(alignment: .bottomTrailing) {
+                Image("Avatar")
+                    .resizable()
+                    .frame(width: 48, height: 48)
+                if isOnline {
+                    Image("AvatarBadge")
+                }
+            }.padding(EdgeInsets(top: 0, leading: 16, bottom: 0, trailing: 0))
+            
+            VStack(alignment: .leading) {
+                Text(message.name)
+                
+                Text(message.text).lineLimit(.zero)
+            }
+            
+            Spacer(minLength: 20)
+            
+            VStack {
+                Text(message.name).frame(maxWidth: 32)
+                
+                Text(message.text)
+                    .lineLimit(1)
+                    .frame(maxWidth: 32)
+            }.padding(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 16))
+        }
     }
 }
 
 struct MessageRow_Previews: PreviewProvider {
     static var previews: some View {
-        MessageRow()
+        MessageRow(message: Message(id: 1, name: "2", text: "3fgdfgdfklgjdfkghdfjkghdfjkdfjkghdfjkghdfjkghdfjkghdfjkghdfjghdfjkhgdfjkghjkdfhgdfjkghdfkjghdfjkghdfkjghdfjkghdfjkghfdkjghdfjkghfdjkghdfjkghjkdfhgjk", time: 4))
     }
 }
